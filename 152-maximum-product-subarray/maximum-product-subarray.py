59dd681e4f21e215a8eb05ac08c76d8cad42fe69
Khushnor Rahman Meem <105982702+meem-5971@users.noreply.github.com>
2026-08-18 12:47:11 +0600

    Time: 6 ms (95.79%) | Memory: 13.3 MB (31.32%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        n = len(nums)
+        pre = 1
+        suff = 1
+        ans = float('-inf')
+
+        for i in range(n):
+            if pre == 0:
+                pre = 1
+            if suff == 0:
+                suff = 1
+
+            pre *= nums[i]
+            suff *= nums[n - i - 1]
+
+            ans = max(ans, pre, suff)
+
+        return ans
